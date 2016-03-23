68e356684c75797f027bb6d2bf102f002528e8fc
Sarmirim <Sarmirim@gmail.com>
2016-03-23 21:10:38 +0300

    1

--- app/src/main/java/com/example/sarmi/clicker_2_2/Tab1.java
+++ app/src/main/java/com/example/sarmi/clicker_2_2/Tab1.java
@@ -4,7 +4,7 @@ package com.example.sarmi.clicker_2_2;
  * Created by sarmi on 19.03.2016.
  */
 
-
+/**/
         import android.os.Bundle;
         import android.support.annotation.Nullable;
         import android.support.v4.app.Fragment;
